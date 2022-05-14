d2d079dc283dc5a9d336459805377d53cdd9f195
godfolabi <103207870+godfolabi@users.noreply.github.com>
2022-05-14 14:16:48 -0400

    Delete VSCode directory

--- VSCode/Week6/jSProgrammingExample.js
+++
@@ -1,3 +0,0 @@
-let name = prompt("What's your name?");
-
-alert(`Hello, ${name}!`);
--- VSCode/Week7/off-canvas_navigation.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('menu-hamburger').addEventListener('click', function () {
-    document.getElementById('menu').classList.add('menu-open');
-})
-document.getElementById('menu-close').addEventListener('click', function () {
-    document.getElementById('menu').classList.remove('menu-open');
-})
